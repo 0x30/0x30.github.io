373318d4bab8b0e5dd3ae570ddca46d784cd368f
jingwenzheng <alphabetion@gmail.com>
2020-11-30 18:25:08 +0800

    feat: 终端样式 博客

--- src/components/chatRoom/index.tsx
+++ src/components/chatRoom/index.tsx
@@ -1,10 +1,105 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, onUnmounted, PropType, ref } from "vue";
 import Style from "./index.module.scss";
+import { init, MESSAGE_TYPE } from "@rongcloud/imlib-v4";
+import xterm, { IDisposable, IEvent, Terminal } from "xterm";
+import xtermAddonFit from "xterm-addon-fit";
 
-export const ChatRoomButton = defineComponent(() => {
-  return () => {
-    return <button class={Style.chatRoomButton}>
-      
-    </button>;
+import Lodash from "lodash";
+
+const { debounce } = Lodash;
+const { FitAddon } = xtermAddonFit;
+
+const logo = `
+ █     █░▒███████▒
+▓█░ █ ░█░▒ ▒ ▒ ▄▀░
+▒█░ █ ░█ ░ ▒ ▄▀▒░ 
+░█░ █ ░█   ▄▀▒   ░
+░░██▒██▓ ▒███████▒
+░ ▓░▒ ▒  ░▒▒ ▓░▒░▒
+  ▒ ░ ░  ░░▒ ▒ ░ ▒
+  ░   ░  ░ ░ ░ ░ ░
+    ░      ░ ░    
+         ░        
+                 
+${new Date().toLocaleString()}                                                                                                      
+`;
+
+const command = ["help", "posts", "user"];
+
+export const ChatRoom = defineComponent(() => {
+  const inputValueRef = ref("$ ");
+
+  const values = ref<String[]>([logo]);
+
+  const inputEvent = (event: Event) => {
+    inputValueRef.value = (event.target as any).innerText;
   };
+
+  const onPrompt = (val: string) => {
+    values.value.push(val);
+    values.value.push("$0 ");
+  };
+
+  return () => {
+    return (
+      <div class={Style.chatRoom}>
+        {values.value.map((val) => (
+          <div class={Style.line}>{val}</div>
+        ))}
+        <InputBody onPrompt={onPrompt}></InputBody>
+      </div>
+    );
+  };
+});
+
+const InputBody = defineComponent({
+  props: {
+    sss: String,
+    onPrompt: {
+      type: Function as PropType<(value: string) => void>,
+      required: true,
+    },
+  },
+  setup: (props) => {
+    /// 输入 组件
+    const inputRef = ref(null);
+    /// 输入的值
+    const inputValueRef = ref("");
+
+    /// 输入事件
+    const inputEvent = (event: Event) => {
+      inputValueRef.value = (event.target as any).innerText;
+    };
+
+    const keyDownHandle = (ev: KeyboardEvent) => {
+      /// enter key board
+      if (ev.keyCode === 13) {
+        props.onPrompt(inputValueRef.value);
+        inputValueRef.value = "";
+        inputRef.value.innerHTML = "";
+        ev.preventDefault();
+      }
+    };
+
+    const onFocusHandler = () =>
+      document.addEventListener("keydown", keyDownHandle, false);
+    const onBlurHandler = () =>
+      document.removeEventListener("keydown", keyDownHandle);
+
+    return () => {
+      return (
+        <div class={Style.inputBody}>
+          <div class={Style.inputPlaceholder}>{inputValueRef.value}3</div>
+          <div
+            ref={inputRef}
+            contenteditable
+            class={Style.inputWrap}
+            onInput={inputEvent}
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+          ></div>
+        </div>
+      );
+    };
+  },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,58 @@
 import { createApp, defineComponent } from "vue";
+import { ChatRoom } from "./components/chatRoom";
 import "./index.css";
 
 const App = defineComponent(() => {
   return () => {
-    return <div></div>;
+    return <ChatRoom></ChatRoom>;
   };
 });
 
 createApp(App).mount("#app");
+
+
+  // onMounted(async () => {
+  //   const token =
+  //     "S8qXI5XUx+MqOjcOvBPF3nDzrtWE/KLs@3uzh.cn.rongnav.com;3uzh.cn.rongcfg.com";
+
+  // const im = init({
+  //   appkey: "pvxdm17jx2xer",
+  // });
+
+  //   await im.connect({ token });
+
+  //   im.watch({
+  //     conversation: function (event) {
+  //       var updatedConversationList = event.updatedConversationList; // 更新的会话列表
+  //       console.log("更新会话汇总:", updatedConversationList);
+  //       // console.log(
+  //       //   "最新会话列表:",
+  //       //   im.Conversation.merge({
+  //       //     conversationList,
+  //       //     updatedConversationList,
+  //       //   })
+  //       // );
+  //     },
+  //     message: function (event) {
+  //       var message = event.message;
+  //       console.log("收到新消息:", message);
+  //     },
+  //     status: function (event) {
+  //       var status = event.status;
+  //       console.log("连接状态码:", status);
+  //     },
+  //   });
+
+  //   // 注: im 实例通过 RongIMLib.init 获取(单个页面仅需初始化一次)
+  //   var chatRoom = im.ChatRoom.get({
+  //     id: "chatRoom1",
+  //   });
+
+  //   chatRoom
+  //     .join({
+  //       count: 20, // 进入后, 自动拉取 20 条聊天室最新消息
+  //     })
+  //     .then(function () {
+  //       console.log("加入聊天室成功");
+  //     });
+  // });
